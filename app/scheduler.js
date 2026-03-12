165e7ca61ec34bd47ba9b9c7207d89e78b5c980f
nikochetov <nikochetov@gmail.com>
2026-03-12 23:36:32 +0300

    update scheduler.js

--- app/scheduler.js
+++ app/scheduler.js
@@ -3,7 +3,7 @@ import cron from "node-cron";
 
 dotenv.config()
 
-const schedulerPattern = '0 8-21 * * *'
+const schedulerPattern = '0 8-21 * * 1-5'
 const timezone = process.env.TIMEZONE
 
 export default (taskFn) => {
